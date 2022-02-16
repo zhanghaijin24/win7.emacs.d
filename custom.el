a7020218ffbe4d7a4a59eff24ba4d8039ca41547
zhanghaijin24 <zhanghaijin24@163.com>
2022-02-16 19:48:58 +0800

    add custom.el

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(awesome-tab-active-bar-height 50)
+ '(package-selected-packages
+   '(magit evil wgrep keycast company use-package vertico orderless monokai-theme marginalia embark consult)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- init.el
+++ init.el
@@ -251,16 +251,5 @@ Supports exporting consult-grep to wgrep, file to wdeired, and consult-location 
 	;;   (add-to-list 'load-path dir))
 	)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(magit evil wgrep keycast company use-package vertico orderless monokai-theme marginalia embark consult)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq custom-file (expand-file-name "~/.emacs.d/custom.el"))
+(load custom-file)
