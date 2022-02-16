b4ffda7a34aa0240cb4b77dc9f67232f41147764
zhanghaijin24 <zhanghaijin24@163.com>
2022-02-16 20:52:04 +0800

    add org config

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(magit evil wgrep keycast company use-package vertico orderless monokai-theme marginalia embark consult)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
