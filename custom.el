f96b8ba3b124eb894b9168dee20929365105a07d
zhanghaijin24 <zhanghaijin24@163.com>
2022-03-01 20:48:15 +0800

    org-capture links win7 fix

--- custom.el
+++ custom.el
@@ -3,14 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-default-notes-file (concat org-directory "/notes.org") nil ((org-hide-leading-stars t)))
+ '(org-default-notes-file (concat org-directory "/notes.org"))
  '(org-directory "~/posts")
  '(org-export-html-postamble nil)
  '(org-hide-leading-stars t)
  '(org-startup-folded 'overview)
  '(org-startup-indented t)
  '(package-selected-packages
-   '(org-ac org-bullets magit evil wgrep keycast company use-package vertico orderless monokai-theme marginalia embark consult)))
+   '(noflet org-ac org-bullets magit evil wgrep keycast company use-package vertico orderless monokai-theme marginalia embark consult)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
